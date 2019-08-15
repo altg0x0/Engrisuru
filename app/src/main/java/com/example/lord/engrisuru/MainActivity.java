b7ea9744e34a88540fb9127e480681f689257bff
altg0x0 <altg0x0@yandex.ru>
2019-08-15 22:59:08 +0300

    Refactor to utilize itemClickListener and avoid code duplication, fixed logic

--- app/src/main/java/com/example/lord/engrisuru/MainActivity.java
+++ app/src/main/java/com/example/lord/engrisuru/MainActivity.java
@@ -23,17 +23,17 @@ public class MainActivity extends AppCompatActivity
         mainActivity = this;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
         if (savedInstanceState != null) return; // Avoid double fragment creation

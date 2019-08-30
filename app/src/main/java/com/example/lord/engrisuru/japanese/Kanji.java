ec15592cfd25108da8d3b7de97176de6f991b441
altg0x0 <altg0x0@yandex.ru>
2019-08-30 22:14:21 +0300

    Add python scrip to create sqlite3 db from kanjidic2

--- app/src/main/java/com/example/lord/engrisuru/japanese/Kanji.java
+++ app/src/main/java/com/example/lord/engrisuru/japanese/Kanji.java
@@ -2,8 +2,8 @@ package com.example.lord.engrisuru.japanese;
 
 public class Kanji {
     public char character; // All kanji are supposedly 'non-astral' unicode
-    public int grade;
-    public String[] onyomiReadings;
-    public String[] kunyomiReadings;
+    public int grade = 10; // 10 == ungraded
+    public String[] onyomiReadings = new String[]{"ソンザイシテイナイ"};
+    public String[] kunyomiReadings = new String[]{"そんざいしていない"};
     public String[] englishMeanings;
 }
--- python/kanjidic2sqlite.py
+++ python/kanjidic2sqlite.py
@@ -1,3 +1,4 @@
+import sqlite3
 try:
     import lxml.etree as etree
 except ImportError:
@@ -5,10 +6,47 @@ except ImportError:
     import xml.etree.ElementTree as etree
 from pathlib import Path
 
+
+def firstOrDefaultText(xpath, default):
+    return xpath[0].text if xpath else default
+
+
+def stringsByNodes(nodesList):
+    return list(map(lambda x: x.text, nodesList))
+
+
 if not Path("kanjidic2.xml").is_file():
     print("Kanjidic xml file not found in the working directory.")
     exit(2)
-
 with open("kanjidic2.xml") as f:
     xmlRoot = etree.parse(f)
     print("Kanjidic file parsed successfully!")
+try:
+    conn = sqlite3.connect("kanji.db")
+    cur = conn.cursor()
+    cur.execute(
+        """CREATE TABLE if not exists kanji ( \
+        character text primary key, grade integer, onyomiReadings text, kunyomiReadings text, englishMeanings text); \
+        """)
+    # cur.execute("insert into kanji values ('1', 2, '1', '2', '3');")
+except sqlite3.Error:
+    print("Problems with sqlite3 database detected.")
+    exit(3)
+charactersNodes = xmlRoot.xpath("//character")
+inserts = []
+for i in charactersNodes:
+    character = i.xpath(".//literal")[0].text  # literal is always present
+    grade = int(firstOrDefaultText(i.xpath(".//grade"), 10))
+    onyomiReadingsList = stringsByNodes(i.xpath(".//reading[@r_type='ja_on']")) or ["ソンザイシテイナイ"]
+    kunyomiReadingsList = stringsByNodes(i.xpath(".//reading[@r_type='ja_kun']")) or ["そんざいしていない"]
+    englishMeaningsList = stringsByNodes(i.xpath(".//meaning[not(@*)]"))
+    inserts.append(
+        (character,
+         grade,
+         ','.join(onyomiReadingsList),
+         ','.join(kunyomiReadingsList),
+         ','.join(englishMeaningsList),))
+# print(inserts[10:20])
+cur.executemany('insert into kanji values (?, ?, ?, ?, ?);', inserts)
+conn.commit()
+cur.close()
